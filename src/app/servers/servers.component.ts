69b1f1c54b0b617f3e0b63c8bd1cd204cdb2a209
Rui Braz <rui.braz.correia@gmail.com>
2020-12-01 15:38:09 -0300

    Finalizando exercicio 2.

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  allowResetUsername = false;
+  serverCreationStatus = 'No server was created.';
+  serverName = '';
+  username = '';
+  
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
