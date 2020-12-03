e8314d7272e750bec22b6dd1aadc7636f83fbc09
Rui Braz <rui.braz.correia@gmail.com>
2020-12-03 02:18:38 -0300

    Falta ultimo quesito do exercicio 3.

--- src/app/exercicio-tres/exercicio-tres.component.ts
+++ src/app/exercicio-tres/exercicio-tres.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExercicioTresComponent implements OnInit {
 
+  showText = false;
+  counter = 0;
+  displays: Array<number> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onToogleDisplay(){
+    if(this.showText){
+      this.showText = false;
+    } else {
+      this.showText = true;
+    }
+    this.onCountClicks();
+    
+  }
+
+  onCountClicks(){
+    this.counter = this.counter + 1;
+    this.displays.push(this.counter);
   }
 
 }
